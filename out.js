3daada5312450f546c650921b2f35721e50fd0e8
Jakub Szczepański <jakub.teodor.szczepanski@gmail.com>
2019-02-10 13:59:37 +0100

    defining fuctions to draw a ball and make it move

--- out.js
+++ out.js
@@ -1,20 +1,22 @@
 var canvas = document.getElementById('myCanvas');
     ctx = canvas.getContext('2d');
+    x = canvas.width/2;
+    y = canvas.height-30;
+    dx = 2;
+    dy = -2;
 
-ctx.beginPath();
-ctx.rect(40, 40, 50, 50);
-ctx.fillStyle = "#000099";
-ctx.fill();
-ctx.closePath();
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 20, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
 
-ctx.beginPath();
-ctx.arc(300, 200, 40, 0, Math.PI*2, false);
-ctx.fillStyle = "yellow";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(200, 40, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, .5)";
-ctx.stroke();
-ctx.closePath();
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+}
+setInterval(draw, 10);
