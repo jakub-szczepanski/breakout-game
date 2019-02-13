cec48362154847cbe6cdd2b53d724153a7a4eec5
Jakub Szczepański <jakub.teodor.szczepanski@gmail.com>
2019-02-13 13:51:58 +0100

    updated collision detection function

--- out.js
+++ out.js
@@ -49,6 +49,9 @@ function collisionDetection() {
     for (var c=0; c<brickColumnCount; c++) {
         for (var r=0; r<brickRowCount; r++) {
             var b = bricks[c][r];
+            if (x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                dy = -dy;
+            }
         }
     }
 }
